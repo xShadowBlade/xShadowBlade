b8c76dc3877d0ed8e902950687a142539d859863
zBlade <dnguy3n2010@gmail.com>
2024-10-01 19:47:38 -0400

    build: update dependencies, eslint, build options

--- meta-app/webpack.config.js
+++ meta-app/webpack.config.js
@@ -1,28 +1,38 @@
+/**
+ * @file Webpack configuration file.
+ */
+/* eslint-disable jsdoc/check-tag-names */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
-const { EsbuildPlugin } = require("esbuild-loader");
 const webpack = require("webpack");
 // in case you run into any typescript error when configuring `devServer`
 require("webpack-dev-server");
+const { EsbuildPlugin } = require("esbuild-loader");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlReplaceWebpackPlugin = require("html-replace-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = (env, argv) => {
-    console.log("Args:", argv);
     const mode = argv.mode;
-    return {
+    /**
+     * @type {import("webpack").Configuration}
+     */
+    const options = {
         entry: "./src/index.tsx", // Entry point of your application
         output: {
-            path: path.resolve(__dirname, "dist"),
+            path: path.resolve(__dirname, "build"),
             filename: "bundle.js", // Output bundle file name
         },
-        // devtool: mode === "development" ? "source-map" : "none",
         resolve: {
             extensions: [".css", ".tsx", ".ts", ".js", "..."],
+            alias: {
+                // "emath.js": "emath.js/ts",
+                // "emath.js/game": "emath.js/ts/game",
+                // "emath.js/presets": "emath.js/ts/presets",
+            },
         },
-        // watch: true,
-        // watchOptions: {
-        //     ignored: "**/node_modules",
-        // },
         module: {
             rules: [
             // Use esbuild to compile JavaScript & TypeScript
@@ -37,49 +47,62 @@ module.exports = (env, argv) => {
                         tsconfig: "./tsconfig.json",
                     },
                 },
-                // {
-                //     test: /\.(ts|tsx)$/,
-                //     use: "ts-loader",
-                //     exclude: /node_modules/,
-                // },
                 {
-                    test: /\.css$/i,
-                    use: ["css-loader"],
+                    // If you enable `experiments.css` or `experiments.futureDefaults`, please uncomment line below
+                    // type: "javascript/auto",
+                    test: /\.(sa|sc|c)ss$/i,
+                    use: [
+                        mode === "development" ? "style-loader" : MiniCssExtractPlugin.loader,
+                        "css-loader",
+                    ],
                 },
             ],
         },
         optimization: {
             minimizer: [
                 new EsbuildPlugin({
-                    target: "es2015", // Syntax to transpile to (see options below for possible values)
+                    target: "es2015", // Syntax to transpile to
                 }),
+                new CssMinimizerPlugin(),
             ],
         },
         plugins: [
             new HtmlWebpackPlugin({
                 template: "./public/index.html", // Use this HTML file as a template
             }),
-            new webpack.DefinePlugin({
-                // "%PUBLIC_URL%": JSON.stringify(mode === "production" ? "../public/" : "./"),
-                MODE: JSON.stringify(mode),
+            // Define global constants
+            new EsbuildPlugin({
+                define: {
+                    MODE: JSON.stringify(mode),
+                },
             }),
-            // mode === "production" ? new webpack.optimize.LimitChunkCountPlugin({
-            //     maxChunks: 1,
-            // }) : null,
-            // new EsbuildPlugin({
-            //     options: {
-            //         define: {
-            //             // "%PUBLIC_URL%": JSON.stringify(mode === "production" ? "../public/" : "./"),
-            //             MODE: JSON.stringify(mode),
-            //         },
-            //     },
-            // }),
             new HtmlReplaceWebpackPlugin([
                 {
                     pattern: "%PUBLIC_URL%",
-                    replacement: mode === "production" ? "../public/" : "./",
+                    // ! This is the public path of your app
+                    replacement: mode === "production" ? "./public/" : "./",
                 },
             ]),
         ],
     };
+    if (mode === "production") {
+        options.plugins.push(
+            // Copies public folder to dist folder
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: "public",
+                        to: "public",
+                        globOptions: {
+                            ignore: ["**/index.html"],
+                        },
+                    },
+                ],
+            }),
+            new MiniCssExtractPlugin(),
+        );
+    } else if (mode === "development") {
+        options.devtool = "eval-cheap-module-source-map";
+    }
+    return options;
 };
