dc2c74ecfdf28d6410fa9d2fffcbd89252412f2c
zBlade <dnguy3n2010@gmail.com>
2025-03-30 17:02:23 -0400

    feat: add nebula-sh

--- src/terminal/terminal.tsx
+++ src/terminal/terminal.tsx
@@ -3,33 +3,101 @@
  */
 import React, { useEffect, useRef } from "react";
 import type { UseXTermProps } from "react-xtermjs";
-import { useXTerm } from "react-xtermjs";
+import { useXTerm, XTerm } from "react-xtermjs";
+
+import { NebulaShAddon } from "nebula-sh";
 
 /**
  * @returns The xterm component.
  */
+// export const Terminal: React.FC = () => {
+//     // Use the xterm instance and ref.
+//     const { instance, ref } = useXTerm({
+//         options: {
+//             // Font and letter
+//             fontFamily: "JetBrains Mono, monospace",
+//             fontSize: 14,
+//             letterSpacing: 1,
+//             lineHeight: 1.4,
+//             fontWeight: "400",
+//             fontWeightBold: "600",
+//             allowTransparency: true,
+
+//             // Cursor
+//             // Disabled because of custom blink css
+//             // cursorBlink: true,
+//             cursorStyle: "block",
+//             cursorInactiveStyle: "block",
+//             altClickMovesCursor: false,
+//             smoothScrollDuration: 100,
+
+//             theme: {
+//                 background: "#151515",
+//                 foreground: "#fff",
+//                 cursor: "#fff",
+
+//                 // ANSI colors (changed to be brighter)
+//                 black: "#333333",
+//                 red: "#ff6e73",
+//                 green: "#98e06a",
+//                 yellow: "#ffd74d",
+//                 blue: "#64a5ff",
+//                 magenta: "#d46dff",
+//                 cyan: "#6ae0ff",
+//                 white: "#eaeaea",
+//             },
+//         },
+//     });
+
+//     // If the instance is not available, return an empty div.
+//     if (!instance) {
+//         return <div />;
+//     }
+
+//     // Add the NebulaSh addon.
+//     instance.loadAddon(new NebulaShAddon());
+
+//     return <div ref={ref} style={{ width: "100%", height: "100%" }} />;
+// };
+
 export const Terminal: React.FC = () => {
-    // Use the xterm instance and ref.
-    const { instance, ref } = useXTerm({
-        // options: {
-        //     cursorBlink: true,
-        //     fontSize: 16,
-        //     fontFamily: "Menlo, Monaco, Consolas, 'Courier New', monospace",
-        //     theme: {
-        //         background: "#000000",
-        //         foreground: "#FFFFFF",
-        //     },
-        // },
-    });
+    return (
+        <XTerm
+            options={{
+                // Font and letter
+                fontFamily: "JetBrains Mono, monospace",
+                fontSize: 14,
+                letterSpacing: 1,
+                lineHeight: 1.4,
+                fontWeight: "400",
+                fontWeightBold: "600",
+                allowTransparency: true,
 
-    // If the instance is not available, return an empty div.
-    if (!instance) {
-        return <div />;
-    }
+                // Cursor
+                // Disabled because of custom blink css
+                // cursorBlink: true,
+                cursorStyle: "block",
+                cursorInactiveStyle: "block",
+                altClickMovesCursor: false,
+                smoothScrollDuration: 100,
 
-    // Write to the terminal.
-    instance.writeln("Hello world!");
-    instance.onData((data) => instance.write(data));
+                theme: {
+                    background: "#151515",
+                    foreground: "#fff",
+                    cursor: "#fff",
 
-    return <div ref={ref} style={{ width: "100%", height: "100%" }} />;
+                    // ANSI colors (changed to be brighter)
+                    black: "#333333",
+                    red: "#ff6e73",
+                    green: "#98e06a",
+                    yellow: "#ffd74d",
+                    blue: "#64a5ff",
+                    magenta: "#d46dff",
+                    cyan: "#6ae0ff",
+                    white: "#eaeaea",
+                },
+            }}
+            addons={[new NebulaShAddon()]}
+        />
+    );
 };
